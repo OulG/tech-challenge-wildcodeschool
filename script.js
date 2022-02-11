75e071c1fe073f840de694afcaddfb557acd6f7c
OulG <lou.gain@outlook.fr>
2022-02-11 19:38:17 +0100

    Fix things

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById("message").innerHTML="Do it, the Kingdom want to help you.";
-
 const message =
   "Thank you for send your message to the kingdom. We will respond as soon as possible. Believe in us.";
 
