7edc20f9e352fe924650882a3bf28d8e1bcb9bda
OulG <lou.gain@outlook.fr>
2022-02-13 15:07:40 +0100

    footer on all pages

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ document
     event.preventDefault();
     alert(message);
   });
+
+
+
+  // document.getElementById("contactForm").innerHTML = "Thank you for send your message to the kingdom. We will respond as soon as possible. Believe in us.";
