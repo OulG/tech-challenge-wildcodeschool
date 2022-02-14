fd7ce63210f89a4a2a51ca64bc2816d7214e0b3f
OulG <lou.gain@outlook.fr>
2022-02-14 11:07:26 +0100

    W3C validator

--- script.js
+++ script.js
@@ -7,7 +7,3 @@ document
     event.preventDefault();
     alert(message);
   });
-
-
-
-  // document.getElementById("contactForm").innerHTML = "Thank you for send your message to the kingdom. We will respond as soon as possible. Believe in us.";
